f1dc5ffdfc483fd98847ae6d61fe47f21965f841
kythonlk <kythonlk@gmail.com>
2025-04-10 10:11:21 +0400

    Update

--- src/components/services/logistics.tsx
+++ src/components/services/logistics.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import { logistic } from '../../lib/services-const';
+import { MoveLeft } from 'lucide-react';
 
 export default function Logistics() {
 
   return (
-    <section id={logistic.id} className="scroll-m-16 bg-[#fdfefe] border-b-1 border-gray-900/10 text-gray-600 shadow-lg">
+    <section id={logistic.id} className="scroll-m-16 bg-[#fdfefe] border-b-1 border-gray-900/10 text-gray-600 shadow-lg pt-12">
       <div className="text-center py-10 p-4">
         <h2 className="text-2xl sm:text-5xl font-semibold mt-4">{logistic.title}</h2>
       </div>
@@ -27,6 +29,14 @@ export default function Logistics() {
             />
           </div>
         ))}
+      </div>
+
+      <div className="py-8 flex justify-center">
+        <Link to={"/services"}>
+          <button className="bg-[#005aa6] text-white px-6 py-2 flex gap-2 rounded-lg hover:bg-[#005aa6] transition">
+            <MoveLeft size={24} /> Go Back to Services
+          </button>
+        </Link>
       </div>
     </section>
   );
